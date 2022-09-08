ef51bec9f49db6e3bb11493d6074f0d00bb36b94
connie <mcostance@gmail.com>
2022-09-08 14:08:38 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
 /**
- *
- *  * print_programming is like building a puzzle
- *
- *   */
-
+ * print_programming is like building a puzzle
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
-
 }
