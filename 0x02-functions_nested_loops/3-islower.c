ed8453527c94a94160fd03b4bab8e4de8d7c0541
connie <mcostance@gmail.com>
2022-09-13 13:28:47 -0700

    print selected letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * int _islower(int c) - print upper or lower
+ * Return: 1 if lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
