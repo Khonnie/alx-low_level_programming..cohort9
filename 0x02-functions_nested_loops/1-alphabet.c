7e303ce6bab1ee6b2d00595e3b7410fe9588e6ec
connie <mcostance@gmail.com>
2022-09-13 06:32:35 -0700

    alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - entry point
+ * print_alphabet - function for alphabet
+ * return: Always 0
+ */
+void print_alphabet(void);
+int main(void);
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	char letter = 'a';
+	while(a <= z)
+	{
+	_putchar(letter);
+	letter++
+	}
+	_putchar('\n');
+}
