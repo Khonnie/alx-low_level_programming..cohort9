b12280386e8110792b99d6a168b058d494a1b80b
connie <mcostance@gmail.com>
2022-09-13 10:58:23 -0700

    alphabet1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,16 @@
 #include "main.h"
 /**
- * main - entry point
- * print_alphabet - function for alphabet
- * return: Always 0
+ * print_alphabet - user defined function
+ * 
+ * Return: Always 0
  */
-void print_alphabet(void);
-int main(void);
-{
-	print_alphabet();
-	return (0);
-}
 void print_alphabet(void)
 {
-	char letter = 'a';
-	while(a <= z)
-	{
-	_putchar(letter);
-	letter++
-	}
-	_putchar('\n');
+char letter = 'a';
+while('a' <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
 }
