4355b91b5d040312fcdb7a1e8758313a84380805
connie <mcostance@gmail.com>
2022-09-13 11:31:27 -0700

    alphabet lower

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
  * print_alphabet - user defined function
- * 
  * Return: Always 0
  */
 void print_alphabet(void)
 {
 char letter = 'a';
-while(letter <= 'z')
+while (letter <= 'z')
 {
 _putchar(letter);
 letter++;
